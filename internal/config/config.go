ef86e2c09416fbc444193282008beb049a6cbef6
Sergei Ampilov <sergei.ampilov@gmail.com>
2026-06-29 21:58:41 +0300

    feat: conn string - connection to db

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -25,8 +26,48 @@ func (c *config) loadFromEnv() {
 		port = "8080" // default port
 	}
 	
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+	
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		dbPort = "5432"
+	}
+	
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "postgres"
+	}
+	
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "postgres"
+	}
+	
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = ""
+	}
+	
+	dbSSLMode := os.Getenv("DB_SSL_MODE")
+	if dbSSLMode == "" {
+		dbSSLMode = "disable"
+	}
+	
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		dbHost, dbPort, dbUser, dbPassword, dbName, dbSSLMode)
+	
 	c.appConfig = &AppConfig{
-		Port: port,
+		Port:       port,
+		DBHost:     dbHost,
+		DBPort:     dbPort,
+		DBName:     dbName,
+		DBUser:     dbUser,
+		DBPassword: dbPassword,
+		DBSSLMode:  dbSSLMode,
+		ConnString: connString,
 	}
 }
 
--- internal/config/model.go
+++ internal/config/model.go
@@ -2,4 +2,11 @@ package config
 
 type AppConfig struct {
 	Port string
+	DBHost     string
+	DBPort     string
+	DBName     string
+	DBUser     string
+	DBPassword string
+	DBSSLMode  string
+	ConnString string
 }
